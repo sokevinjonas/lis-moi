7fee7a19a3e98986c5f10e77b575a473fbb511f6
ao627515 <ao627515@gmail.com>
2024-12-26 23:03:30 +0000

    `Added click event to toggle sidebar and implemented toggleSidebar method in HeaderComponent`

--- src/app/admin/components/header/header.component.ts
+++ src/app/admin/components/header/header.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/core/services/global/global.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    return;
+  }
+
+  toggleSidebar() {
+    document.body.classList.toggle('toggle-sidebar');
+  }
 
 }
